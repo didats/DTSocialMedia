27e5c7168b741d9b634541a2bff3faad3444458a
Didats Triadi <didats@gmail.com>
2013-07-15 16:17:21 +0300

    Updated sample script

--- sample.php
+++ sample.php
@@ -2,11 +2,11 @@
 	require_once("DTSocialMedia.php");
 	$social = new DTSocialMedia();
 	
-	$social->consumerkey = "DEYdPPGUpYqJTDNgw6sUw";
-	$social->consumersecret = "6HzaqpQy8T4j90CY7o4LQmK76rY0TDNkRxLX7pEwlI";
-	$social->accesstoken = "51386840-gm7rjpq6LHLChB2aerZdwzdTk3agqG5Gh2Z5mknKQ";
-	$social->accesstokensecret = "jjRL8fjoCZ03blMCrKprm2dn3uW16ZZRiU0NrK2pw9I";
+	$social->consumerkey = "";
+	$social->consumersecret = "";
+	$social->accesstoken = "";
+	$social->accesstokensecret = "";
 	
-	$data = $social->get_data("twitter", "VivaTelecom", 20);
+	$data = $social->get_data("twitter", "didats", 20);
 	
 	print_r($data);
